ff8cec3ef736b5a51ff1d324f9903cb70cc3a5d2
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-06-15 21:52:57 +0200

    fixes

--- cpuminer-multi.rb
+++ cpuminer-multi.rb
@@ -1,8 +1,9 @@
 require "formula"
 
 class CpuminerMulti < Formula
-  homepage "https://github.com/LucasJones/cpuminer-multi"
-  head "https://github.com/LucasJones/cpuminer-multi.git"
+  homepage "https://github.com/lucasjones/cpuminer-multi"
+  head "https://github.com/lucasjones/cpuminer-multi.git"
+  url "https://github.com/lucasjones/cpuminer-multi/archive/v1.0.3.zip"
 
   depends_on "automake" => :build
   depends_on "autoconf" => :build
@@ -12,7 +13,7 @@ class CpuminerMulti < Formula
   def install
     system "./autogen.sh"
     system "perl nomacro.pl"
-    system "./configure CFLAGS=\"-march=native\" US_CPPFLAGS= \"-03\""
+    system "./configure CFLAGS=\"-march=native\"" # US_CPPFLAGS= \"-03\""
     system "make"
     bin.install "cpuminer"
   end
