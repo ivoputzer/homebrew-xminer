27e4c475c6b098d6bed3cc0b6bbbd0cbb35688f0
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-06-15 22:02:47 +0200

    removes curl dependency

--- cpuminer-multi.rb
+++ cpuminer-multi.rb
@@ -9,7 +9,7 @@ class CpuminerMulti < Formula
   depends_on "autoconf" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl" => :build
-  depends_on "curl" => :build
+  # depends_on "curl" => :build
   depends_on "libtool" => :build
   depends_on "jansson"
 
