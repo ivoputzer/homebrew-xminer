5dc96ff49bec4d038e524de42321d20e44fa3d8f
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-06-15 21:09:55 +0200

    fixes quotes

--- bfgminer.rb
+++ bfgminer.rb
@@ -1,22 +1,22 @@
-require 'formula'
+require "formula"
 
 class Bfgminer < Formula
-  homepage 'https://github.com/luke-jr/bfgminer'
-  head 'https://github.com/luke-jr/bfgminer.git', :branch => 'bfgminer'
-  url 'http://luke.dashjr.org/programs/bitcoin/files/bfgminer/5.1.0/bfgminer-5.1.0.zip'
-  sha256 '7b56f7f59dbb019972641dc252372c5235e0b6433bdcfd6f6966d3087346bbe2'
+  homepage "https://github.com/luke-jr/bfgminer"
+  head "https://github.com/luke-jr/bfgminer.git", :branch => "bfgminer"
+  url "http://luke.dashjr.org/programs/bitcoin/files/bfgminer/5.1.0/bfgminer-5.1.0.zip"
+  sha256 "7b56f7f59dbb019972641dc252372c5235e0b6433bdcfd6f6966d3087346bbe2"
 
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
-  depends_on 'pkg-config' => :build
-  depends_on 'uthash' => :build
-  depends_on 'curl'
-  depends_on 'jansson'
-  depends_on 'libmicrohttpd'
-  depends_on 'libevent'
-  depends_on 'libusb'
-  depends_on 'hidapi'
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "uthash" => :build
+  depends_on "curl"
+  depends_on "jansson"
+  depends_on "libmicrohttpd"
+  depends_on "libevent"
+  depends_on "libusb"
+  depends_on "hidapi"
 
   def install
     system "NOSUBMODULES=1 ./autogen.sh"
