0188de4e44cdfa9ff7ca89371556c8f69642f1c9
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-06-15 22:13:46 +0200

    patches sources

--- cpuminer-multi.rb
+++ cpuminer-multi.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class CpuminerMulti < Formula
   homepage "https://github.com/lucasjones/cpuminer-multi"
-  head "https://github.com/lucasjones/cpuminer-multi.git"
-  url "https://github.com/lucasjones/cpuminer-multi/archive/v1.0.3.zip"
+  head "https://github.com/ivoputzer/cpuminer-multi", :branch => "patch-1"
+  url "https://github.com/ivoputzer/cpuminer-multi/archive/patch-1.zip"
 
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl" => :build
-  # depends_on "curl" => :build
+  depends_on "curl" => :build
   depends_on "libtool" => :build
   depends_on "jansson"
 
